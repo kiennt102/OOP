4d0d13933541437510a0be512ac4aebc1199d3bb
Kien Nguyen Trung <134778703+kiennt102@users.noreply.github.com>
2023-09-17 22:31:02 +0700

    HW2

--- Program.cs
+++ Program.cs
@@ -4,15 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HW1
+namespace HW2
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
-			profile Kiennt = new profile();
-			Kiennt.Show();
-            Console.WriteLine(Kiennt.Introduce());
+            Console.WriteLine("===== Management Person Programe =====");
+            Console.Write("The number of person: ");int n = int.Parse(Console.ReadLine());
+			Person[] per = new Person[n];
+			for(int i = 0; i < n; i++)
+			{
+                Console.WriteLine($"Enter information for person {i+1}:");
+                per[i] = Person.inputPersonInfor();
+			}
+			Person per1 = new Person("alex","St 19",8);
+			Person.displayPerson(per1);
+
+			per= Person.SortedBySala(per);
+			foreach(var s in per)
+			{
+				Person.displayPerson(s);
+			}
+
+
             Console.ReadKey();
 		}
 	}
